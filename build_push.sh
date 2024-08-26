276fe82183e214e38c6e0a665303264f878b1f1a
plaquette <winkler.janos@gmail.com>
2024-08-26 11:28:35 +0200

    clean up

--- build_push.sh
+++ build_push.sh
@@ -3,8 +3,5 @@ docker build -t plaquette/panx:dev .
 # # start an interactive session inside the container
 # docker run -it plaquette/panx:dev
 
-# test
-# panX.py -h
-# panX.py -fn pan-genome-analysis/data/TestSet -sl TestSet -t 2
 
 # docker push plaquette/panx:dev 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,3 @@ conda activate panX
 exec "$@"
 
 
-
-# #!/bin/bash
-
-# # Source the conda setup script
-# source /miniconda/etc/profile.d/conda.sh
-
-
-# # Activate the panX environment
-# conda activate panXpy3
-
-# # Execute the command passed to the script or start a bash shell if none is passed
-# exec "$@"
-
-
-
